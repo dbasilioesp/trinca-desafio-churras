31d88a42643ab833a0af0dd175ce1c71bd0231bd
David Basilio <dbasilioesp@gmail.com>
2018-08-31 17:40:18 -0300

    Fixing favicon

--- src/app/App/App.js
+++ src/app/App/App.js
@@ -12,7 +12,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoggedIn: true
+      isLoggedIn: false
     };
     this.handleLogin = this.handleLogin.bind(this);
   }
